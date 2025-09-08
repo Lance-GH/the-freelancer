5f240d6f280b7795321ac7df1cb071eb17252e6d
Lance <20822340@sun.ac.za>
2025-09-08 13:29:00 +0200

    mongodb warning fix

--- app.js
+++ app.js
@@ -15,12 +15,9 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public'))); // serve frontend
 
 // --- MongoDB setup ---
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error('MongoDB error:', err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB error:', err));
 
 // --- Cloudinary setup ---
 cloudinary.config({
